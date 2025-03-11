8a19cfc720f06504bc4320b6d8046afcad981788
andhitanh <onecodhita@gmail.com>
2025-03-11 10:24:59 +0700

    feat: "add env variable for api url"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const apiUrl = import.meta.env.VITE_API_URL;
+
 function App() {
+  console.log("API URL:", apiUrl)
   const [count, setCount] = useState(0)
 
   return (
