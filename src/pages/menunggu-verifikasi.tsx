42a2c2d9876c2054d520ea11b8562784a82d89c7
andhitanh <onecodhita@gmail.com>
2025-03-16 09:47:19 +0700

    feat : menunggu verifikasi page

---
+++ src/pages/menunggu-verifikasi.tsx
@@ -0,0 +1,31 @@
+import { Clock } from 'lucide-react';
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
+
+const WaitVerification = () => {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-[#E7DECD] gap-8">
+        <img className="w-50" src="/logo-green.svg" alt="logo-salman"/>
+        <Card className='w-65 h-auto md:w-120 md:h-80'>
+            <CardHeader className="flex flex-col justify-center items-center text-center">
+                <Clock className="text-[#3A786D] w-16 h-16 md:w-25 md:h-25"/>
+                <CardTitle className=" text-xl md:text-3xl">Menunggu Verifikasi</CardTitle>
+                <CardDescription className='text-sm md:text-base'>Dokumen anda telah berhasil di unggah. Menunggu verifikasi oleh pihak SSR.</CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <Button className='w-full mt-4 bg-[#3A786D] hover:bg-black text-[#FBFAF8]'>KEMBALI</Button>
+            </CardFooter>
+        </Card>
+    </div>
+  )
+}
+
+export default WaitVerification;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import Login from "@/pages/login";
 import Register from "@/pages/register";
 import RegisterDataDiri from "@/pages/register-datadiri";
+import WaitVerification from "@/pages/menunggu-verifikasi";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
@@ -10,6 +11,7 @@ const AppRoutes = () => (
       <Route path="/login" element={<Login />}></Route>
       <Route path="/register" element={<Register />}></Route>
       <Route path="/register-datadiri" element={<RegisterDataDiri />}></Route>
+      <Route path="/wait-verification" element={<WaitVerification />}/>
     </Routes>
   </BrowserRouter>
 );
