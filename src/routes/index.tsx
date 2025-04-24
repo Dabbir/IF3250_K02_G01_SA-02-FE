f6d5730e90675e283477336cae734debda0230df
nanthedom <naufnan@gmail.com>
2025-04-24 16:50:51 +0700

    feat: stakeholder page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -17,6 +17,7 @@ import DetailProgram from "@/pages/detailprogram.tsx";
 import DetailPublikasi from "@/pages/detailpublikasi.tsx";
 import AdminDashboard from "@/pages/admin-dashboard.tsx";
 import ViewerAccessManagement from "@/pages/viewer-access.tsx";
+import StakeholderPage from "@/pages/stakeholders.tsx";
 
 const AppRoutes = () => (
   <BrowserRouter>
@@ -71,7 +72,7 @@ const AppRoutes = () => (
         <Route path="/publikasi" element={<Publikasi />} />
         <Route path="/kegiatan" element={<Kegiatan />} />
         <Route path="/kegiatan/:id" element={<DetailKegiatan />} />
-        <Route path="/stakeholder" element={<div className="p-4">Stakeholder Content</div>} />
+        <Route path="/stakeholder" element={<StakeholderPage />} />
         <Route path="/penerima-manfaat" element={<div className="p-4">Penerima Manfaat Content</div>} />
         <Route path="/karyawan" element={<div className="p-4">Karyawan Content</div>} />
         <Route path="/galeri" element={<div className="p-4">Galeri Content</div>} />
