c4b56d82e3ce3d174629255e3f9f9a86f8407b4b
nanthedom <naufnan@gmail.com>
2025-04-24 18:20:23 +0700

    fix: status enum

--- src/pages/admin-dashboard.tsx
+++ src/pages/admin-dashboard.tsx
@@ -473,6 +473,7 @@ export default function AdminDashboard() {
         year: "numeric" 
       });
     } catch (e) {
+      console.log("Error parsing date:", e);
       return dateString;
     }
   };
